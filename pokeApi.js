4c6448d6fe74e6b35706035bfe09fc9cce02c0ee
Reistoge <64452194+Reistoge@users.noreply.github.com>
2025-09-02 10:34:20 -0400

    Initial commit

---
+++ pokeApi.js
@@ -0,0 +1,11 @@
+const cardTemplate = document.getElementById("card-prefab");
+const cardsCointainer = document.getElementById("cards-container");
+
+// const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+// const request = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+// await fetch(request)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error('Error:', error));
+ 
